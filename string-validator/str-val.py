ed5ce6344b7f2dae288455d3dd62f79b6c5e1e6f
Zack Dean <zack.dean@gmail.com>
2021-03-23 10:07:18 -0400

    new challenge

---
+++ string-validator/str-val.py
@@ -0,0 +1,24 @@
+!#/bin/python 
+
+if __name__ == '__main__':
+    s = input()
+    if s.isalnum():
+        print ("True")
+    else:
+        print ("False")
+    if s.isalpha():
+        print ("True")
+    else:
+        print ("False")
+    if s.isdigit():
+        print ("True")
+    else:
+        print ("False")
+    if s.islower():
+        print ("True")
+    else:
+        print ("False")
+    if s.isupper():
+        print ("True")
+    else:
+        print ("False")
