d632d833eb03f50963024cfd79246085f15fc261
Zack Dean <zack.dean@gmail.com>
2021-03-25 18:24:08 -0400

    Solved

--- string-validator/str-val.py
+++ string-validator/str-val.py
@@ -1,24 +1,15 @@
-!#/bin/python 
+#!/bin/python 
 
 if __name__ == '__main__':
-    s = input()
-    if s.isalnum():
-        print ("True")
-    else:
-        print ("False")
-    if s.isalpha():
-        print ("True")
-    else:
-        print ("False")
-    if s.isdigit():
-        print ("True")
-    else:
-        print ("False")
-    if s.islower():
-        print ("True")
-    else:
-        print ("False")
-    if s.isupper():
-        print ("True")
-    else:
-        print ("False")
+    string = input()
+
+    isAlnum = any(char.isalnum() for char in string)
+    isAlpha = any(char.isalpha() for char in string) 
+    isDigit = any(char.isdigit() for char in string)
+    isLower = any(char.islower() for char in string)
+    isUpper = any(char.isupper() for char in string)
+    print (isAlnum)
+    print (isAlpha)
+    print (isDigit) 
+    print (isLower)
+    print (isUpper) 
