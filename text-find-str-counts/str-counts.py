547bc1ad4c22948aa2f4dd96d91f298133bc134b
Zack Dean <zack.dean@gmail.com>
2021-03-29 08:39:16 -0400

    Working on it

--- text-find-str-counts/str-counts.py
+++ text-find-str-counts/str-counts.py
@@ -2,8 +2,15 @@
 import re 
 
 def count_substring(string, sub_string):
-    pattern = sub_string
-    return len(re.findall(pattern, sub_string)) 
+    count = 0 
+    left = 0 
+    while True:
+        match = re.search(string, sub_string)
+        if not match:
+            break
+        count += 1
+        left += match.start() + 1
+    return count 
 
 if __name__ == '__main__':
     string = input().strip()
