cc3e2fbe7ee7b4451215a39874c4af0ea51c15e4
Zack Dean <zack.dean@gmail.com>
2021-03-31 23:26:11 -0400

    saving progress

--- find-percentage/percentage.py
+++ find-percentage/percentage.py
@@ -1,10 +1,19 @@
-#!/bin/bash 
+#!/bin/python
+
+def generateAverage(sm, qName):
+    avg = 0.00 
+    
+    return avg 
 
 if __name__ == '__main__':
     n = int(input())
     student_marks = {}
+    average = []
     for _ in range(n):
         name, *line = input().split()
         scores = list(map(float, line))
         student_marks[name] = scores
     query_name = input()
+    
+    
+
