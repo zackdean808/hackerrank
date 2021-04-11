f2828dba4d678fcdc44ba1bf2ddbe604686631fa
Zack Dean <zack.dean@gmail.com>
2021-04-11 11:22:41 -0400

    New problem

--- nested-lists/nested-lists.py
+++ nested-lists/nested-lists.py
@@ -12,9 +12,24 @@ if __name__ == '__main__':
     students.sort(key = lambda x: x[1])
     print (students)
 
-    i = 0
+    
+    
+    uniqStudents = set()
+
+    for s in students:
+        uniqStudents.add(s)
+
+    for i in students: 
+        if not i in uniqStudents:
+            print ("True")
+        else:
+            print("False") 
+
+    """
+    di = 0
     while i < len(students): 
         if arr[i][0][1] == arr[i + 1][0][1]
             print (sort(arr[i][0][1], arr[i+1][0][1]) 
             i+=1
         elif arr[i][1] < 
+    """
