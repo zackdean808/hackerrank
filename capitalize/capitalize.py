a5f2cf65490634fe732177c945145fa44a3b5199
Zack Dean <zack.dean@gmail.com>
2021-04-25 16:48:51 -0400

    Testing

--- capitalize/capitalize.py
+++ capitalize/capitalize.py
@@ -1,14 +1,20 @@
 #!/bin/python 
 
+
+
 def solve(s):
+    # Algo: 
+    # split string on the space, save to splitStr list 
+    # iterate through each item, call builtin capitalize 
+    # Save to temp list t 
+    # save string to list to string var ltos 
+
     t = []
     splitStr = s.split()
     for i in splitStr:
-        #print (i.capitalize())
-        #t.append(i.capitalize())
-        t.append(i)
-
-    return s.join(t) 
+        t.append(i.capitalize())
+        ltos = ' '.join(map(str, t))
+    return ltos 
 
 if __name__ == '__main__':
     s = str(input())
