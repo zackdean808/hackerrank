4b1230fe40bc4f0821a87d18801ced35802da2e5
Zack Dean <zack.dean@gmail.com>
2021-04-27 19:14:52 -0400

    working on it

--- validate-and-parse-email-address/validator.py
+++ validate-and-parse-email-address/validator.py
@@ -1,20 +1,28 @@
-#!/bin/bash 
+#!/bin/python 
 
-
+import re
 
 def solve(s):
-    pattern = "[a-zA-Z]{1,}[a-zA-Z0-9-_.]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}"
-    if re.match(pattern, s):
+    pattern = re.compile("\<[a-zA-Z]{1,}[a-zA-Z0-9-_.]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}\>")
+    t = s.split()
+    print (t[1])
+    if re.match(pattern, t[1]):
         return "YES"
-    elif not re.match(pattern, s):
+        print ("YES")
+        print (t[1])
+    elif not re.match(pattern, t[1]):
         return "NO"
+        print ("NO")
+        print (t[1])
     else:
         sys.exit(1)
+    t.clear()
 
 if __name__ == '__main__':
     N = int(input())
     for i in range(0, N):
-        s = tuple(input())
+        s = str(input())
+
         result = solve(s)
         print (result)
 
