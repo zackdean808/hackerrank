a6bc663f316057eed1d4e88c0a04d96c8cb703a7
Zack Dean <zack.dean@gmail.com>
2021-04-27 23:01:07 -0400

    still working

--- string-formatted/formatted.py
+++ string-formatted/formatted.py
@@ -2,11 +2,15 @@
 
 def print_formatted(number):
     for i in range(1, number + 1):
-        dec = i
-        octal = oct(i)[2:]
-        hexadec = hex(i)[2:]
-        binary = bin(i)[2:]
-        print (dec,"\t",octal,"\t",hexadec,"\t",binary)
+        dec = str(i)
+        octal = str(oct(i)[2:])
+        hexadec = str(hex(i)[2:]).upper()
+        binary = str(bin(i)[2:])
+        width = len(binary) 
+        s = dec + "\t" + octal + "\t" + hexadec + "\t" + binary
+        #s = dec.rjust(width) + octal.rjust(width) + hexadec.rjust(width) + binary.rjust(width)
+        print (s) 
+        #print (dec,"\t",octal,"\t",hexadec,"\t",binary)
     
 if __name__ == '__main__':
     n = int(input())
