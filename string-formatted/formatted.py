e206f4de72edfe845d9d45300ec1033027ff084e
Zack Dean <zack.dean@gmail.com>
2021-04-29 17:55:29 -0400

    Solved

--- string-formatted/formatted.py
+++ string-formatted/formatted.py
@@ -1,13 +1,16 @@
 #!/bin/python 
 
 def print_formatted(number):
+    w = len(bin(number)[2:])
+
     for i in range(1, number + 1):
         dec = str(i)
         octal = str(oct(i)[2:])
         hexadec = str(hex(i)[2:]).upper()
         binary = str(bin(i)[2:])
         width = len(binary) 
-        s = dec + "\t" + octal + "\t" + hexadec + "\t" + binary
+        
+        s = dec.rjust(w, ' ') + "\t" + octal.rjust(w, ' ') + "\t" + hexadec.rjust(w, ' ') + "\t" + binary.rjust(w, ' ')
         #s = dec.rjust(width) + octal.rjust(width) + hexadec.rjust(width) + binary.rjust(width)
         print (s) 
         #print (dec,"\t",octal,"\t",hexadec,"\t",binary)
