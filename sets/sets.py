bb02ae794a1d1d7c1bc8f12139bd3a4474988208
Zack Dean <zack.dean@gmail.com>
2021-05-11 11:15:06 -0400

    Solved

--- sets/sets.py
+++ sets/sets.py
@@ -1,49 +1,38 @@
 #!/bin/python 
 
 if __name__ == '__main__':
-    append = "append"
-    insert = "insert"
-    sort = "sort"
+    
+    discard = "discard" 
     pop = "pop"
-    reverse = "reverse"
     remove = "remove"
-    l = []
-    tempList = []
-
+    
+    # Read in the length of the set
+    numberOfItems = int(input())
+    # Read in the set and discard any space/newline char 
+    s = set(map(int, input().split()))
+    s.discard(' ')
     # Read in the times to run the foor loop 
-    N = int(input())
-
-    for i in range(0, N):
+    numberOfCommands = int(input())
+    total = 0 
+    
+    for i in range(0, numberOfCommands):
         line = str(input())
-
-        if append in line:
-            #print ("appened")
-            # Bug by getting string -1, this did not get double digits 
-            # Just grabbed the last element in the split string cast as int 
-            # then append 
-            tempList = line.split()
-            l.append(int(tempList[-1]))
-            tempList.clear()
-        elif insert in line:
-            #print ("insert") 
-            tempList = line.split()
-            l.insert(int(tempList[1]),int(tempList[2]))
-            tempList.clear()
-            #change this to  a split and use that. 
-        elif sort in line:
-            #print ("sort")
-            l.sort()
-        elif pop in line:
+        line.strip()
+        if discard in line:
             #print ("pop") 
-            l.pop(-1)
-        elif reverse in line:
-           # print ("reverse")
-           l.reverse()
+            s.discard(int(line[-1]))
         elif remove in line:
             #print ("remove")
-            l.remove(int(line[-1]))
-        elif "print" in line:
-            #print ("print")
-            print (l)
+            try:
+                s.remove(int(line[-1]))
+            except:
+                pass 
+        elif pop in line:
+            s.pop()
         else:
             print ("something went wrong")
+
+    for i in s:
+        total += i
+
+    print(total) 
